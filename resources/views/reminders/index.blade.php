ac7d664e1f3851129a4950e772ed628071aa08c1
Christophe <christophe@xbookmarks.local>
2026-07-09 13:53:02 +0200

    fix: surface context and reminder workflows

--- resources/views/reminders/index.blade.php
+++ resources/views/reminders/index.blade.php
@@ -73,7 +73,7 @@
                     </article>
                 @empty
                     <div class="rounded-lg border border-dashed border-neutral-300 p-6 text-sm text-neutral-600 dark:border-neutral-700 dark:text-neutral-400">
-                    {{ __('Nog geen herinneringen.') }}
+                        {{ __('Nog geen herinneringen.') }}
                     </div>
                 @endforelse
             </div>
@@ -93,7 +93,7 @@
                     </article>
                 @empty
                     <div class="rounded-lg border border-dashed border-neutral-300 p-6 text-sm text-neutral-600 dark:border-neutral-700 dark:text-neutral-400">
-                    {{ __('Nog geen afgewerkte herinneringen.') }}
+                        {{ __('Nog geen afgewerkte herinneringen.') }}
                     </div>
                 @endforelse
             </div>
